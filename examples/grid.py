010af584be3ce3985eec8f2b3de1570c0355cdda
Zhi <zhihuahan72@gmail.com>
2023-05-31 15:49:55 +0300

    add damage grid function

--- examples/grid.py
+++ examples/grid.py
@@ -1,11 +1,12 @@
 from graph_state import GraphState
 from collections import Counter
+import random
 import networkx as nx
 
 # Use 30 qubits
 results = []
-height = 5
-width = 5 #should be same as height for now
+height = 2
+width = 2 #should be same as height for now
 dicts = []
 
 class Grid(GraphState):
@@ -14,22 +15,44 @@ class Grid(GraphState):
         self.width = width
         super().__init__(width*height)
         self.edges = []
+        self.removed = []
         
-    def make_grid(self):
         for j in range(self.height):
             for i in range(self.width-1):
                 self.edges.append((i+j*self.width, (i+1)+j*self.width))
 
         for j in range(self.height-1):
             for i in range(self.width):
-                self.edges.append((i+j*width, i+(j+1)*width))
+                self.edges.append((i+j*self.width, i+(j+1)*self.width))
 
+        for i in range(self.height*self.width):
+            self.h(i)
+        
         for e in self.edges:
-            self.add_edge(*e)
+            self.cz(*e)
+
+
+
+    def damage_grid(self, p):
+        # p is the probability of losing a qubit
+        for i in range(self.height*self.width):
+            if random.random() < p:
+                g.measure(i)
+                self.removed.append(i)
+
 
 
 g = Grid(height, width)
-g.make_grid()
+
+g.measure(1)
+
+
+n = g.to_networkx()
+for i in g.removed:
+    n.remove_node(i)
+print(nx.to_numpy_array(n))
+
+
 print(nx.to_numpy_array(g.to_networkx()))
 g.draw()
 #N = nx.to_numpy_array(g.to_networkx())
