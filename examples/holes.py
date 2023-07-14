b3062be6f866dd334d5ef8195d94815b3376bd8e
Zhi <zhihuahan72@gmail.com>
2023-07-14 16:24:16 +0300

    Removed useless code

--- examples/holes.py
+++ examples/holes.py
@@ -26,50 +26,17 @@ class Holes:
         if graph_add_node:
             self.graph.add_node(i)
 
-    def are_nodes_connected(self, node1, node2):
-        x1 = np.array(self.node_coords[node1])
-        x2 = np.array(self.node_coords[node2])
-
-        if np.sum(np.abs(x1 - x2)) == 1:
-            return True
-        else:
-            return False
-
     def add_edges(self):
         nodes = list(self.graph.nodes)
         for index, n in enumerate(nodes):
             for n2 in nodes[index:]:
-                if self.are_nodes_connected(n, n2):
+                if taxicab_metric(n, n2) == 1:
                     self.graph.add_edge(n, n2)
         self.double_hole()
 
     def to_networkx(self):
         self.add_edges()
         return self.graph
-
-    def return_plot_data(self):
-        x_nodes = [self.node_coords[j][0] for j in self.graph.nodes] # x-coordinates of nodes
-        y_nodes = [self.node_coords[j][1] for j in self.graph.nodes] # y-coordinates
-        z_nodes = [self.node_coords[j][2] for j in self.graph.nodes] # z-coordinates
-
-        #we need to create lists that contain the starting and ending coordinates of each edge.
-        x_edges=[]
-        y_edges=[]
-        z_edges=[]
-
-        #need to fill these with all of the coordinates
-        for edge in self.graph.edges:
-            #format: [beginning,ending,None]
-            x_coords = [self.node_coords[edge[0]][0],self.node_coords[edge[1]][0],None]
-            x_edges += x_coords
-
-            y_coords = [self.node_coords[edge[0]][1],self.node_coords[edge[1]][1],None]
-            y_edges += y_coords
-
-            z_coords = [self.node_coords[edge[0]][2],self.node_coords[edge[1]][2],None]
-            z_edges += z_coords
-        
-        return [x_nodes, y_nodes, z_nodes], [x_edges, y_edges, z_edges]
 
     def double_hole(self):
         """
